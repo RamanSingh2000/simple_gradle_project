3e1da45d889cedc6354d215ba585b395933d1bd1
RamanSingh2000 <145480618+RamanSingh2000@users.noreply.github.com>
2023-11-05 17:43:09 +0530

    Update Application.java

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
 	
-	@GetMapping
+	
 	public String message(){
 		return "Hello Dev Team.Hurray!We have deployed the latest build for gradle";
 	}
