531e3ad7483d1e68736492733a74a1f4d206d8a4
Raman Singh <Raman.Singh@blueconchtech.com>
2023-11-05 17:33:53 +0530

    first commit

---
+++ src/main/java/com/example/Application.java
@@ -0,0 +1,18 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	
+	@GetMapping
+	public String message(){
+		return "Hello Dev Team.Hurray!We have deployed the latest build for gradle";
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
