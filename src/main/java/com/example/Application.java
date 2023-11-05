e64f23dff7bd38fe9e6a7a3bf6d9bbe9e142fbfc
RamanSingh2000 <145480618+RamanSingh2000@users.noreply.github.com>
2023-11-05 20:29:43 +0530

    Update Application.java

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -2,12 +2,14 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
+@RestController	
 public class Application {
 
 	
-	
+	@RequestMapping
 	public String message(){
 		return "Hello Dev Team.Hurray!We have deployed the latest build for gradle";
 	}
